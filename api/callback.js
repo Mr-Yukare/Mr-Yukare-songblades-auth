f894d329d8c512994a9f086cbc249fd8e1becdb0
Mr-Yukare <yukarefr@gmail.com>
2026-06-07 21:37:03 +0200

    Update callback.js

--- api/callback.js
+++ api/callback.js
@@ -2,9 +2,8 @@ export default function handler(req, res) {
   const { code, state } = req.query;
   
   if (code) {
-    // Rediriger vers l'appli avec le code
-    res.redirect(`songbladesapp://auth?code=${code}&state=${state || ''}`);
+    res.redirect(302, `songbladesapp://auth?code=${code}&state=${state || ''}`);
   } else {
-    res.status(400).json({ error: 'No code provided' });
+    res.status(400).send('No code provided');
   }
 }
